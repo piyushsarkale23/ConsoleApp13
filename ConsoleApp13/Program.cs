4483e36fc0cc1d109e2d5f942606b45b1271d2cc
piyushsarkale23 <piyushsarkale35@gmail.com>
2023-04-05 18:17:35 +0530

    Add project files.

---
+++ ConsoleApp13/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //int i = 0;
+            //int pos = 0;
+            //int[] arr = new int[10];
+
+            ////Read numbers into array
+            //Console.WriteLine("Enter numbers : ");
+            //for (i = 0; i < 5; i++)
+            //{
+            //    Console.Write("Element[" + (i + 1) + "]: ");
+            //    arr[i] = int.Parse(Console.ReadLine());
+            //}
+
+
+            //Console.Write("Enter position to delete item : ");
+            //pos = int.Parse(Console.ReadLine());
+
+            ////Perform shift opearation
+            //for (i = pos - 1; i < 5; i++)
+            //{
+            //    arr[i] = arr[i + 1];
+            //}
+
+
+            ////print array after deletion
+            //Console.WriteLine("Array elements after deletion : ");
+            //for (i = 0; i < 4; i++)
+            //{
+            //    Console.WriteLine("Element[" + (i + 1) + "]: " + arr[i]);
+            //}
+
+            ////print array after deletion
+            //Console.WriteLine("Array elements after deletion : ");
+
+
+            //check the number is armstrong or not\
+
+            //int n, r, sum = 0, temp;
+            //Console.Write("Enter the Number= ");
+            //n = int.Parse(Console.ReadLine());
+            //temp = n; //371
+            //while (n > 0)
+            //{
+            //    r = n % 10;  //1  // 7  //  //3
+            //    sum = sum + (r * r * r);  //1  //343  //371
+            //    n = n / 10;  //37  //3  //0
+            //}
+            //if (temp == sum)
+            //    Console.Write("Armstrong Number.");
+            //else
+            //    Console.Write("Not Armstrong Number.");
+            //calculate factorial of num
+            //int num, x;
+            //Console.WriteLine("Enter the Number ");
+            //num = int.Parse(Console.ReadLine());
+            //Console.WriteLine("The Factors are : ");
+            //for (x = 1; x <= num; x++)
+            //{
+            //    if (num % x == 0)
+            //    {
+            //        Console.WriteLine(x);
+            //    }
+            //}
+            //Console.ReadLine();
+            //calculate fiaboncii series
+            //int val1 = 0, val2 = 1, val3, i, n;
+            //n = 74;
+            //Console.WriteLine("Fibonacci series:");
+            //Console.Write(val1 + " " + val2 + " ");
+            //for (i = 2; i < n; ++i)
+            //{
+            //    val3 = val1 + val2;
+            //    Console.Write(val3 + " ");
+            //    val1 = val2;
+            //    val2 = val3;
+            //}
+        }
+
+
+    }
+}
+
+
